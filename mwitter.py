604c81134ebe12fb63c731e9b02316f87e26e2d6
yattom <tsutomu.yasui@gmail.com>
2018-06-11 21:37:41 +0900

    tweet

--- mwitter.py
+++ mwitter.py
@@ -1,6 +1,6 @@
 # cofing: utf-8
 
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 app = Flask(__name__)
 
 class User:
@@ -18,19 +18,38 @@ class User:
         return [u for u in User.users if u.name == name][0]
 
 
+class Tweet:
+    tweets = []
+
+    @staticmethod
+    def 新規作成(user, text):
+        tweet = Tweet()
+        tweet.author = user
+        tweet.text = text
+        Tweet.tweets.append(tweet)
+        print(Tweet.tweets)
+        return tweet
+
+
 User.新規作成('自分')
 
 
 @app.route("/")
 def index():
-    return render_template('index.html', users=User.users, tweets=[])
-
+    return render_template('index.html', users=User.users, tweets=Tweet.tweets)
 
 @app.route("/users/<name>/timeline")
 def users_timeline(name):
-    return render_template('index.html', user=User.取得(name), users=[], tweets=[])
+    return render_template('timeline.html', user=User.取得(name), tweets=Tweet.tweets)
 
 @app.route("/users/create", methods=['POST'])
 def create_user():
     User.新規作成(request.form['name'])
     return redirect('/')
+
+@app.route("/users/<name>/tweets/create", methods=['POST'])
+def create_tweet(name):
+    user = User.取得(name)
+    tweet = Tweet.新規作成(user=user, text=request.form['text'])
+    return redirect(url_for('users_timeline', name=user.name))
+
