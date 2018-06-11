874b4425a534f47a832a9efa0a84f49e5316901e
yattom <tsutomu.yasui@gmail.com>
2018-06-11 22:13:29 +0900

    login

--- mwitter.py
+++ mwitter.py
@@ -1,7 +1,8 @@
 # cofing: utf-8
 
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 app = Flask(__name__)
+app.secret_key = "THISISNOTASECRET"
 
 class User:
     users = []
@@ -44,9 +45,14 @@ def index():
     tweets = Tweet.tweets
     return render_template('index.html', users=users, tweets=tweets)
 
-@app.route("/users/<name>/timeline")
-def users_timeline(name):
-    user=User.取得(name)
+@app.route("/login")
+def login():
+    session['username'] = request.args.get('login')
+    return redirect('timeline')
+
+@app.route("/timeline")
+def users_timeline():
+    user=User.取得(session['username'])
     tweets = Tweet.tweets
     return render_template('timeline.html', user=user, tweets=tweets)
 
@@ -65,5 +71,11 @@ def create_tweet(name):
 def user(name):
     user=User.取得(name)
     tweets = Tweet.取得(author=user)
-    return render_template('timeline.html', user=user, tweets=tweets)
+    return render_template('user.html', user=user, tweets=tweets)
+
+@app.route("/users/<name>/followings", methods=['POST'])
+def follow_user(name):
+    user = User.取得(name)
+    tweet = Tweet.新規作成(user=user, text=request.form['text'])
+    return redirect(url_for('users_timeline', name=user.name))
 
