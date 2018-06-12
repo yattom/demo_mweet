04b98dcf6a44a74ddd64c4c7a724e836353335d3
yattom <tsutomu.yasui@gmail.com>
2018-06-12 11:11:57 +0900

    scenario 001

--- mwitter.py
+++ mwitter.py
@@ -9,10 +9,37 @@ class User:
 
     @staticmethod
     def 新規作成(name):
-        pass
+        user = User()
+        user.name = name
+        User.users.append(user)
+        return user
+
+    @staticmethod
+    def 取得(name):
+        return [u for u in User.users if u.name == name][0]
+
+    def ツイートする(self, text):
+        tweet = Tweet.新規作成(self, text)
+        return tweet
+
+    def 自分のツイート(self):
+        return Tweet.tweet_from_user(self)
 
 class Tweet:
     tweets = []
+
+    @staticmethod
+    def 新規作成(author, text):
+        tweet = Tweet()
+        tweet.author = author
+        tweet.text = text
+        Tweet.tweets.append(tweet)
+        return tweet
+
+    @staticmethod
+    def tweet_from_user(author):
+        return [t for t in Tweet.tweets if t.author == author]
+
 
 class Follow:
     follows = []
@@ -22,7 +49,7 @@ User.新規作成('自分')
 
 @app.route("/")
 def index():
-    users = []
+    users = User.users
     tweets = []
     return render_template('index.html', users=users, tweets=tweets)
 
@@ -33,8 +60,8 @@ def login():
 
 @app.route("/timeline")
 def users_timeline():
-    user = None
-    tweets = []
+    user = User.取得(session['login'])
+    tweets = user.自分のツイート()
     return render_template('timeline.html', user=user, tweets=tweets)
 
 @app.route("/users/create", methods=['POST'])
@@ -43,6 +70,8 @@ def create_user():
 
 @app.route("/users/<name>/tweets/create", methods=['POST'])
 def create_tweet(name):
+    user = User.取得(name)
+    tweet = user.ツイートする(request.form['text'])
     return redirect(url_for('users_timeline', name=user.name))
 
 @app.route("/users/<name>")
